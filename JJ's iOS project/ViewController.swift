045900392cd3453fd7b543da7c070d535f6b36ee
hennessy12 <xuqi22alianqiu@gmail.com>
2025-05-04 21:40:52 -0700

    Initial Commit

---
+++ JJ's iOS project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JJ's iOS project
+//
+//  Created by Claire on 2025/5/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
